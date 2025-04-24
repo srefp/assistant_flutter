0eac3d5c2500b0a21937320668491e8139835e88
srefp <44335527+srefp@users.noreply.github.com>
2025-04-25 01:06:02 +0800

    feat: 新分析的半自动延迟

--- lib/util/half_tp.dart
+++ lib/util/half_tp.dart
@@ -1,10 +1,23 @@
+import 'dart:math';
+
 import 'package:assistant/auto_gui/key_mouse_util.dart';
 import 'package:assistant/config/record_config.dart';
+import 'package:flutter_auto_gui_windows/flutter_auto_gui_windows.dart';
+
+final _api = FlutterAutoGuiWindows();
 
 void halfTp() async {
-  await KeyMouseUtil.click();
-  await KeyMouseUtil.click();
-  await Future.delayed(Duration(milliseconds: 10));
-  await KeyMouseUtil.clickAtPoint(RecordConfig.to.getConfirmPosition());
-  await KeyMouseUtil.clickAtPoint(RecordConfig.to.getConfirmPosition());
+  await _api.click(clicks: 2);
+  var point = RecordConfig.to.getConfirmPosition();
+  var res = KeyMouseUtil.physicalPos(point);
+  await _api.moveTo(point: Point(res[0], res[1]));
+  await _api.click(clicks: 1);
+  await _api.click(clicks: 1);
+  await Future.delayed(Duration(milliseconds: 90));
+  await _api.click(clicks: 1);
+
+  // await KeyMouseUtil.click();
+  // await KeyMouseUtil.clickAtPoint(RecordConfig.to.getConfirmPosition());
+  // await KeyMouseUtil.click();
+  // await KeyMouseUtil.clickAtPoint(RecordConfig.to.getConfirmPosition());
 }
